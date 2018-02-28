11ef1205b33ee412f5e6c482b24d7d8a32326493
xuguanghappy <277060070@qq.com>
2018-02-28 15:45:32 +0800

    templete

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -7,30 +7,59 @@
       placeholder="接下来去做什么？"
       @keyup.enter="addTodo"
       >
+      <Item :todo="todo"></Item>
+      <Tabs :filter="filter"></Tabs>
   </section>
 </template>
 <script>
+import Item from './item.vue'
+import Tabs from './tabs.vue'
 export default {
+  data(){
+    return{
+      todo: {
+        id: 0,
+        content: 'this is todo',
+        compoleted: false,
+      },
+      filter: 'all'
+    }
+  },
+  components:{
+    Item,
+    Tabs
+  },
   methods: {
-    addTodo(){}
+    addTodo() {}
   }
-}
+};
 </script>
 <style>
-  .real-app{
-    width: 660px;
-    margin: 0 auto;
-    box-shadow: 0 0 5px #999
-  }
-  .add-input{
-    position: relative;
-    border-bottom: 1px solid rgba(0,0,0,0.06);
-    height: 60px;
-    width: 660px;
-    font-size: 24px;
-    font-weight: 100;
-    padding: 15px 60px 15px 15px;
-  }
+.real-app {
+  width: 660px;
+  margin: 0 auto;
+  box-shadow: 0 0 5px #666;
+}
+.add-input {
+  position: relative;
+  margin: 0;
+  width: 100%;
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
+  border: 0;
+  outline: none;
+  color: inherit;
+  padding: 6px;
+  border: 1px solid #999;
+  box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  font-smoothing: antialiased;
+  padding: 16px 16px 16px 60px;
+  border: none;
+  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+}
 </style>
 
 
