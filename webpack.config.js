a19449718ae96a092038b42815762f26ad53a16c
xuguanghappy <277060070@qq.com>
2018-03-01 15:41:23 +0800

    对打包进行了优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-const ExtracPlugin =  require('extract-text-webpack-plugin')
+const ExtracPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -10,27 +10,55 @@ config = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist') 
+    path: path.join(__dirname, 'dist')
   },
- module: {
-  rules: [
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    },
-    {
-      test: /\.jsx$/,
-      loader: 'babel-loader'
-    },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jepg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
+  ]
+}
+
+if (isDev) {
+  config.module.rules.push(
     {
       test: /\.css$/,
       use: [
-        'style-loader',        
+        'style-loader',
         'css-loader'
       ]
-    },
+    }
+  )
+  config.module.rules.push(
     {
-      test: /\.styl/,
+      test: /\.styl$/,
       use: [
         'style-loader',
         'css-loader',
@@ -42,32 +70,7 @@ config = {
         },
         'stylus-loader'
       ]
-    },
-    {
-      test: /\.(gif|jpg|jepg|png|svg)$/,
-      use: [
-        {
-          loader: 'url-loader',
-          options: {
-            limit: 1024,
-            name: '[name].[ext]'
-          }
-        }
-      ]
-    }
-  ]
- },
- plugins: [
-   new webpack.DefinePlugin({
-     'process.env': {
-       NODE_ENV: isDev ? '"development"' : '"production"'
-     }
-   }),
-   new HTMLPlugin()
- ]
-}
-
-if(isDev) {
+    })
   config.devtool = '#cheap-module-eval-source-map'//这个配置 用来让浏览器可以调试源码
   config.devServer = {
     port: 8000,
@@ -82,6 +85,50 @@ if(isDev) {
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),//配合hot 
     new webpack.NoEmitOnErrorsPlugin()//配合hot 使用
+  )
+} else {
+  config.entry = {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  }
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push(
+    {
+      test: /\.css$/,
+      use: ExtracPlugin.extract({
+        fallback: 'style-loader',
+        use: [        
+          'css-loader'
+        ]
+      })
+    },
+  )
+  config.module.rules.push(
+    {
+      test: /\.styl$/,
+      use: ExtracPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    },
+  )
+  config.plugins.push(
+    new ExtracPlugin('styles.[contentHash:8].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
+    })
   )
 }
 
